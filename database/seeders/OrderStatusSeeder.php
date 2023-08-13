51e3d481a6499475358d012ca078ec05c0f5eb05
Hladgerd <hladgerd@gmail.com>
2023-08-13 20:14:27 +0200

    seed order statuses

--- database/seeders/OrderStatusSeeder.php
+++ database/seeders/OrderStatusSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\OrderStatus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class OrderStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        OrderStatus::insert([
+            ['status' => 'Pending'],
+            ['status' => 'Finalised'],
+        ]);
     }
 }
