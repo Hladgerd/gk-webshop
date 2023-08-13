1be8d6220c7ea712eae8138c27d6a290a31e3cc1
Hladgerd <hladgerd@gmail.com>
2023-08-13 13:39:43 +0200

    add Shop component to dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,20 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import Shop from '@/Components/Shop.vue';
 </script>
 
 <template>
-    <AppLayout title="Buy">
+    <AppLayout title="Shop">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Marketplace
+                Shop
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <Shop />
                 </div>
             </div>
         </div>
