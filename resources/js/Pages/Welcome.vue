3a1a092d1a177e18fa35965512fded4635ae6598
Hladgerd <hladgerd@gmail.com>
2023-08-13 10:31:05 +0200

    fix text in sell div

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1903,7 +1903,7 @@ defineProps({
                         <div>
                             <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">BUY</h2>
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Buy beautifull cats, dogs and everything they need.
+                                Buy beautiful cats, dogs and everything they need.
                             </p>
                         </div>
                     </Link>
@@ -1912,7 +1912,7 @@ defineProps({
                             <div>
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">BUY</h2>
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Buy beautifull cats, dogs and everything they need.
+                                    Buy beautiful cats, dogs and everything they need.
                                 </p>
                             </div>
                         </Link>
@@ -1921,9 +1921,9 @@ defineProps({
 
                     <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                         <div>
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">BUY</h2>
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">SELL</h2>
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                Buy beautiful cats, dogs and everything they need.
+                                Sell the never used cat toys or dog cloths.
                             </p>
                         </div>
                     </Link>
