a6730051ab3f99364f96cdcab9fe985054ef4808
Hladgerd <hladgerd@gmail.com>
2023-08-14 03:01:10 +0200

    add cart index view

--- resources/js/Pages/Dashboard/Cart/Index.vue
+++ resources/js/Pages/Dashboard/Cart/Index.vue
@@ -3,7 +3,7 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 </script>
 
 <template>
-    <AppLayout title="Shop">
+    <AppLayout title="Cart">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 I want to buy these products
@@ -11,9 +11,20 @@ import AppLayout from '@/Layouts/AppLayout.vue';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    Selected products listed
+            <div class="max-w-7xl mx-auto px-4 py-4 space-y-4 sm:px-6 md:flex md:space-y-0 md:space-x-4 lg:px-8">
+                <div class="flex-1">
+                    <div class="flex flex-col items-center mb-2 md:flex-row md:justify-between">
+                        <p class="text-red-600 text-2xl font-semibold" v-if="$page.props.cartCount <= 0">
+                            Your cart is empty!
+                        </p>
+                        <p class="text-red-600 text-2xl font-semibold" v-else>
+                            {{ $page.props.cartCount }} item(s) in cart
+                        </p>
+                        <Link :href="route('dashboard')" class="underline hover:text-red-700 transition">Continue Shopping</Link>
+                    </div>
+                </div>
+                <div class="flex-1">
+
                 </div>
             </div>
         </div>
